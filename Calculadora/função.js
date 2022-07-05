7f6c191b578d8c97ae5ea03a7588332bb4b19de5
Mendax <marcoslucaspereira1@gmail.com>
2022-07-04 21:45:56 -0300

    Nova versao

---
+++ Calculadora/função.js
@@ -0,0 +1,22 @@
+function soma(a,b){
+    return a + b;
+}
+
+function mult(c,d){
+    return c * d;
+}
+
+function div(e,f){
+    return e / f;
+}
+
+function sub(g,h){
+    return g - h;
+}
+
+module.exports = {
+    soma,
+    mult,
+    div,
+    sub,
+}
---
+++ Calculadora/resultado.js
@@ -0,0 +1,7 @@
+var calculadora = require ("./função.js")
+
+
+console.log(calculadora.soma(15,20));
+console.log(calculadora.mult(1,2));
+console.log(calculadora.div(10,10));
+console.log(calculadora.sub(10,10));
